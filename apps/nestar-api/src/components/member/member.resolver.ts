086934f36e2d5f2885523307881b6620475543e1
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-08-12 22:16:39 +0900

    feat: create member schema model

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -6,30 +6,30 @@ export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {}
 
 	/** --------------------------- signup --------------------------- **/
-	@Mutation(() => String)
+	@Mutation(() => String) // @Mutation(POST)
 	public async signup(): Promise<string> {
 		console.log('Mutation: signup');
 		return this.memberService.signup();
 	}
 
 	/** --------------------------- login --------------------------- **/
-	@Mutation(() => String)
+	@Mutation(() => String) // @Mutation(POST)
 	public async login(): Promise<string> {
 		console.log('Mutation: login');
 		return this.memberService.login();
 	}
 
 	/** --------------------------- updateMember --------------------------- **/
-	@Mutation(() => String)
+	@Mutation(() => String) // @Mutation(POST)
 	public async updateMember(): Promise<string> {
 		console.log('Mutation: updateMember');
 		return this.memberService.updateMember();
 	}
 
 	/** --------------------------- getMember --------------------------- **/
-	@Query(() => String)
+	@Query(() => String) // @Query (GET)
 	public async getMember(): Promise<string> {
-		console.log('Mutation: getMember');
+		console.log('Query: getMember');
 		return this.memberService.getMember();
 	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class MemberService {
+	/** --------------------------- mongoose --------------------------- **/
+	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
 	/** --------------------------- signup --------------------------- **/
 	public async signup(): Promise<string> {
 		return 'signup executed';
