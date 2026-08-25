ee300e7ef7e62bfd0235afe1524bf68112ccacde
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-08-25 12:10:00 +0900

    fix: modify view module to record visits

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -11,6 +11,7 @@ import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { MemberUpdate } from '../../libs/dto/member/memer.update';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
 export class MemberResolver {
@@ -65,11 +66,13 @@ export class MemberResolver {
 	}
 
 	/** --------------------------- getMember --------------------------- **/
+	@UseGuards(WithoutGuard)
 	@Query(() => Member) // @Query (GET)
-	public async getMember(@Args('memberId') input: string): Promise<Member> {
+	public async getMember(@Args('memberId') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Member> {
 		console.log('Query: getMember');
+
 		const targetId = shapeIntoMongoObjectId(input);
-		return this.memberService.getMember(targetId);
+		return this.memberService.getMember(memberId, targetId);
 	}
 
 	/** --------------------------- getAllMembersByAdmin --------------------------- **/
