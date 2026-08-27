607de33bbd20ac6096b272752b12398aa3985de7
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-08-27 12:39:46 +0900

    fix: modify upload images configurations via graphqlUploadExpress

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -7,7 +7,7 @@ export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'm
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
 
-export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/webp'];
 export const getSerialForImage = (filename: string) => {
 	const ext = path.parse(filename).ext;
 	return uuidv4() + ext;
